107fdda24e2799270c96806d232918aadf3612cf
DESKTOP-PNN9Q1O\Administrator <justinvo.16.01@outlook.com>
2017-07-14 14:55:03 +0700

    Fixed "GetTimesheetManager" view

--- TSM/Data/ModelViews/LeaveVM.cs
+++ TSM/Data/ModelViews/LeaveVM.cs
@@ -1,19 +1,36 @@
-﻿using TSM.Data.Models;
-using TSM.Models;
-
+﻿using System.ComponentModel.DataAnnotations;
+using TSM.Data.Models;
 namespace TSM.Data.ModelViews
 {
     public class LeaveVM
     {
         public string LeaveID { get; set; }
+
+        [Display(Name = "Employee")]
         public string UserName { get; set; }
+
+        [Display(Name = "From Date")]
         public string FromDate { get; set; }
+
+        [Display(Name = "To date")]
         public string ToDate { get; set; }
-		public string ApprovedDate { get; set; }
+
+        [Display(Name = "Approved/Rejected Date")]
+        public string ApprovedDate { get; set; }
+
+        [Display(Name = "Submitted Date")]
         public string SubmittedDate { get; set; }
+
+        [Display(Name = "Work Shift")]
         public Leave.eWorkShift WorkShift { get; set; }
+
+        [Display(Name = "Leave Type")]
         public string LeaveType { get; set; }
-		public Leave.eState State { get; set; }
+
+        [Display(Name = "State")]
+        public Leave.eState State { get; set; }
+
+        [Display(Name = "Note")]
         public string Note { get; set; }
     }
 }

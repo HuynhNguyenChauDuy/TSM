6f1c39e4d62ed094c4214340f2ecbbfd99810573
DESKTOP-HFMCP40\MMZ <phuc210495@gmail.com>
2017-07-31 08:58:24 +0700

    Save codes before make SignalR

---
+++ TSM/Hubs/myHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace TSM.Hubs
+{
+    public class myHub:Hub
+    {
+        public void ServerListener(string UserName)
+        {
+            Clients.Others.notifyUpdates($"Got Connected");
+        }
+    }
+}
